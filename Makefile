544f6927c6c7dfe583dec4796a5e81f14293fac3
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-26 08:02:31 +0300

    established pgsql connection

--- Makefile
+++ Makefile
@@ -22,5 +22,8 @@ jwt-secret:
 heroku-git:
 	heroku git:remote -a oleksiivelychkolumenjwtauth
 
+pg-info:
+	heroku pg:info
+
 run:
 	php -S localhost:8000 -t public
--- config/database.php
+++ config/database.php
@@ -1,6 +1,18 @@
 <?php
 
+$dbConnection = env('DB_CONNECTION', 'pgsql');
+if ($dbConnection === 'pgsql') {
+    $url = parse_url($dbConnection);
+
+    $hostname   = $url['host'];
+    $username   = $url['user'];
+    $password   = $url['pass'];
+    $database   = substr($url['path'], 1);
+}
+
 return [
+    'migrations' => 'migrations',
+
     'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
@@ -14,18 +26,17 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'driver'            => 'pgsql',
+            'host'              => $hostname ?? null,
+            'port'              => '5432',
+            'database'          => $database ?? null,
+            'username'          => $username ?? null,
+            'password'          => $password ?? null,
+            'charset'           => 'utf8',
+            'prefix'            => '',
+            'prefix_indexes'    => true,
+            'search_path'       => 'public',
+            'sslmode'           => 'prefer',
         ],
     ]
 ];
